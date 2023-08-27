c3eee6b652d441cac3dfffba626210aa9af901cb
Crystal Green <crissybrown1992@icloud.com>
2023-08-26 23:24:08 -0400

    added inquire prompts and made sample readme outline

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
 const inquirer=require('inquirer');
 const fs=require('fs');
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please give an introduction and description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What are the intstructions or guidelines for usage?'
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'What are the installation instructions?'
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'How can someone contribute to this project?'
+    }
+
+])
+
+
