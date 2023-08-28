639d56d010cabcd6a9f0c54deaa8dff3b4c27caf
Crystal Green <crissybrown1992@icloud.com>
2023-08-27 23:20:31 -0400

    Built template literal for readme and fs writefile.

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 const inquirer=require('inquirer');
 const fs=require('fs');
 
+const generateReadMe=({title, description,installation, usage, credits, contribute})=>
+
+`# ${title}
+
+## Description
+${description}
+
+## Table of Contents
+
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Credits](#credits)
+4. [How to Contribute](#how-to-contribute)
+5. [Tests](#tests)
+6. [Questions](#questions)
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## Credits
+${credits}
+
+## How to Contribute
+${contribute}
+
+## Tests
+${tests}
+
+## License
+${license}
+
+## Questions`
+
 inquirer
 .prompt([
     {
@@ -26,14 +62,28 @@ inquirer
     {
         type: 'input',
         name: 'credits',
-        message: 'What are the installation instructions?'
+        message: 'Did you use any sources for your project?  If so, please include them here '
     },
     {
         type: 'input',
         name: 'contribute',
         message: 'How can someone contribute to this project?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please use the arrow keys to select the license used for this project.',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3']
     }
 
 ])
+
+.then((answers) => {
+    const readMeContent = generateReadMe(answers);
+    fs.writeFile('reamdme.md', readMeContent, (err) =>
+        err ? console.error(err) : console.log(`Success! Your README has been gnerated for your project, ${title}`)
+    );
+
+});
 
 
